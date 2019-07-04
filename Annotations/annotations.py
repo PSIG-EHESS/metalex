5be16a3f55a25d952cf72a382cde291891d8794d
FloChiff <45393720+FloChiff@users.noreply.github.com>
2019-07-04 10:27:54 +0200

    Update annotations.py

--- Annotations/annotations.py
+++ Annotations/annotations.py
@@ -1,4 +1,4 @@
-import sys, os, re, dicomots
+import sys, os, re, dico_num
 #Import des modules pour la ligne de commande, le travail sur un dossier et les expressions régulières
 #Import également du dictionnaire qui contiendra les dictionnaires de mot avec leur identifiant pour chaque langue
 
@@ -9,7 +9,7 @@ for roots, dir, files in os.walk(sys.argv[1]):
 		print("reading from "+sys.argv[1] + filename)
 		file_out = open(sys.argv[2] + filename, mode='w')
 		print("writing to "+ sys.argv[2] + filename)
-		dico_termes = eval("dicomots." + sys.argv[3])
+		dico_termes = eval("dico_num." + sys.argv[3])
 		#On appelle ici le dictionnaire et la variable à l'intérieur qui nous interessera en fonction de la langue
 		for texte in file_in.readlines():
 			texte = re.sub("</w>", '<txm:ana resp="#txm" type="#mlx">none</txm:ana></w>', texte)
