b3e462b4c25e9a02309426af40d72185148b0d77
FloChiff <45393720+FloChiff@users.noreply.github.com>
2019-07-04 10:45:50 +0200

    Update script3.sh

--- Nettoyage de texte/script3.sh
+++ Nettoyage de texte/script3.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-python3 3_mise_en_forme_bis.py ../../Chapitre-30-31-36/Transcriptions/ang/En/ ../../Normalisation/En/Chap30-31-36/
-python3 3_mise_en_forme_bis.py ../../Chapitre-30-31-36/Transcriptions/all/All/ ../../Normalisation/All/
-python3 3_mise_en_forme_bis.py ../../Chapitre-30-31-36/Transcriptions/it/It/ ../../Normalisation/It/Chap30-31-36/
-python3 3_mise_en_forme_bis.py ../../Chapitre-30-31-36/Transcriptions/fr/Fr/ ../../Normalisation/Fr/Chap30-31-36/
-#python3 3_mise_en_forme_bis.py ../../Intro-Chap1-2-3/Transcriptions/En/ ../../Normalisation/En/Intro-Chap1-2-3/
-#python3 3_mise_en_forme_bis.py ../../Intro-Chap1-2-3/Transcriptions/Fr/ ../../Normalisation/Fr/Intro-Chap1-2-3/
+python3 3_mise_en_forme_bis.py ../../Chapitre-30-31-36/Transcriptions/En/ ../../Normalisation/En/Chap30-31-36/
+python3 3_mise_en_forme_bis.py ../../Chapitre-30-31-36/Transcriptions/All/ ../../Normalisation/All/Chap30-31-36/
+python3 3_mise_en_forme_bis.py ../../Chapitre-30-31-36/Transcriptions/It/ ../../Normalisation/It/Chap30-31-36/
+python3 3_mise_en_forme_bis.py ../../Chapitre-30-31-36/Transcriptions/Fr/ ../../Normalisation/Fr/Chap30-31-36/
+#La requête est exactement la même que pour le script 1, seul le nom du script change
