58d8735b1f32ec33f453230d7f4e096cbb4d9ba7
FloChiff <45393720+FloChiff@users.noreply.github.com>
2019-07-04 10:57:10 +0200

    Update script_annotations.sh

--- Annotations/script_annotations.sh
+++ Annotations/script_annotations.sh
@@ -2,4 +2,4 @@
 python3 annotations.py ../../Travail\ TXM/Corpus/Norm/Fr/CHAP30/ ../../Travail\ TXM/Corpus/Num/ termes_fr
 python3 annotations.py ../../Travail\ TXM/Corpus/Norm/En/CHAP30/ ../../Travail\ TXM/Corpus/Num/ termes_en
 python3 annotations.py ../../Travail\ TXM/Corpus/Norm/It/CHAP30/ ../../Travail\ TXM/Corpus/Num/ termes_it
-#On appelle ici le script, puis le dossier d'entrée et de sortie et enfin une variable contenu dans le dictionnaire importé dans le script
+#On appelle ici le script, le dossier d'entrée, de sortie et enfin une variable contenue dans le dictionnaire importé dans le script
