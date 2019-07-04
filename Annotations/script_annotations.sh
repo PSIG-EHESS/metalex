bd39bc765e5a2c35fafcd2b9cf975c5ede6af9ba
FloChiff <45393720+FloChiff@users.noreply.github.com>
2019-07-04 10:26:46 +0200

    Add files via upload

---
+++ Annotations/script_annotations.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#python3 annotations.py ../../Travail\ TXM/Corpus/Norm/Fr/CHAP30/ ../../Travail\ TXM/Corpus/Alpha/ termes_fr
+#python3 annotations.py ../../Travail\ TXM/Corpus/Norm/En/CHAP30/ ../../Travail\ TXM/Corpus/Alpha/ termes_en
+#python3 annotations.py ../../Travail\ TXM/Corpus/Norm/It/CHAP30/ ../../Travail\ TXM/Corpus/Alpha/ termes_it
+python3 annotations.py ../../Travail\ TXM/Corpus/Norm/Fr/CHAP30/ ../../Travail\ TXM/Corpus/Num/ termes_fr
+python3 annotations.py ../../Travail\ TXM/Corpus/Norm/En/CHAP30/ ../../Travail\ TXM/Corpus/Num/ termes_en
+python3 annotations.py ../../Travail\ TXM/Corpus/Norm/It/CHAP30/ ../../Travail\ TXM/Corpus/Num/ termes_it
