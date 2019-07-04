8ed4f33b3c772b845da793025923bf1ec6bb5981
FloChiff <45393720+FloChiff@users.noreply.github.com>
2019-07-04 10:37:51 +0200

    Update script1.sh

--- Nettoyage de texte/script1.sh
+++ Nettoyage de texte/script1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-python3 1_mise_en_forme.py ../../Chapitre-30-31-36/Transcriptions/ang/En/ ../../Normalisation/En/Chap30-31-36/
-python3 1_mise_en_forme.py ../../Chapitre-30-31-36/Transcriptions/all/All/ ../../Normalisation/All/
-python3 1_mise_en_forme.py ../../Chapitre-30-31-36/Transcriptions/fr/Fr/ ../../Normalisation/Fr/Chap30-31-36/
-python3 1_mise_en_forme.py ../../Chapitre-30-31-36/Transcriptions/it/It/ ../../Normalisation/It/Chap30-31-36/
-#python3 1_normalisation_pre_dico.py ../../Intro-Chap1-2-3/Transcriptions/En/ ../../Normalisation/En/Intro-Chap1-2-3/
-#python3 1_normalisation_pre_dico.py ../../Intro-Chap1-2-3/Transcriptions/Fr/ ../../Normalisation/Fr/Intro-Chap1-2-3/
+python3 1_mise_en_forme.py ../../Chapitre-30-31-36/Transcriptions/En/ ../../Normalisation/En/Chap30-31-36/
+python3 1_mise_en_forme.py ../../Chapitre-30-31-36/Transcriptions/All/ ../../Normalisation/All/Chap30-31-36/
+python3 1_mise_en_forme.py ../../Chapitre-30-31-36/Transcriptions/Fr/ ../../Normalisation/Fr/Chap30-31-36/
+python3 1_mise_en_forme.py ../../Chapitre-30-31-36/Transcriptions/It/ ../../Normalisation/It/Chap30-31-36/
+#Pour le premier script, la ligne du terminal à rentrer est assez simple
+#Il suffit d'appeler le script puis le dossier d'entrée et le dossier de sortie, nommé ainsi sur mon ordinateur
