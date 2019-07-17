c8bfb63b50921fc2e40bde4f4ce3b2292f6a31f4
FloChiff <45393720+FloChiff@users.noreply.github.com>
2019-07-17 16:32:45 +0200

    Update script_annotations.sh

--- Annotations/script_annotations.sh
+++ Annotations/script_annotations.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+#Utilisation du script contenant les expressions régulières
 python3 annotations.py ../../Travail\ TXM/Corpus/Norm/Fr/CHAP30/ ../../Travail\ TXM/Corpus/Num/ termes_fr
 python3 annotations.py ../../Travail\ TXM/Corpus/Norm/En/CHAP30/ ../../Travail\ TXM/Corpus/Num/ termes_en
 python3 annotations.py ../../Travail\ TXM/Corpus/Norm/It/CHAP30/ ../../Travail\ TXM/Corpus/Num/ termes_it
+#Utilisation du script contenant le module XML pour Python
+python3 script_xml_annotation.py ../../Travail\ TXM/Corpus/Norm/Fr/CHAP30/ ../../Travail\ TXM/Corpus/Num/ termes_fr
+python3 script_xml_annotation.py ../../Travail\ TXM/Corpus/Norm/En/CHAP30/ ../../Travail\ TXM/Corpus/Num/ termes_en
+python3 script_xml_annotation.py ../../Travail\ TXM/Corpus/Norm/It/CHAP30/ ../../Travail\ TXM/Corpus/Num/ termes_it
 #On appelle ici le script, le dossier d'entrée, de sortie et enfin une variable contenue dans le dictionnaire importé dans le script
