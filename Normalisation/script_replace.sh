8cd80d235c4c1fde09fa65df2d95631fbab2dd4b
FloChiff <45393720+FloChiff@users.noreply.github.com>
2019-08-28 17:44:31 +0200

    Update script_replace.sh

--- Normalisation/script_replace.sh
+++ Normalisation/script_replace.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #Utilisation du script avec les expressions régulières
-python3 forme_norm.py ../../../TXM-0.8.0/corpora/FR303136/txm/FR303136/ ../../Travail\ TXM/Corpus/Norm/Fr/CHAP30 remplacement_fr
-python3 forme_norm.py ../../../TXM-0.8.0/corpora/EN303136/txm/EN303136/ ../../Travail\ TXM/Corpus/Norm/En/CHAP30 remplacement_en
-python3 forme_norm.py ../../../TXM-0.8.0/corpora/IT303136/txm/IT303136/ ../../Travail\ TXM/Corpus/Norm/It/CHAP30 remplacement_it
+python3 script_regex_normalisation.py ../../../TXM-0.8.0/corpora/FR303136/txm/FR303136/ ../../Travail\ TXM/Corpus/Norm/Fr/CHAP30 remplacement_fr
+python3 script_regex_normalisation.py ../../../TXM-0.8.0/corpora/EN303136/txm/EN303136/ ../../Travail\ TXM/Corpus/Norm/En/CHAP30 remplacement_en
+python3 script_regex_normalisation.py ../../../TXM-0.8.0/corpora/IT303136/txm/IT303136/ ../../Travail\ TXM/Corpus/Norm/It/CHAP30 remplacement_it
 #Utilisation du script avec le module XML pour Python
 python3 script_xml_normalisation.py ../../../TXM-0.8.0/corpora/FR303136/txm/FR303136/ ../../Travail\ TXM/Corpus/Norm/Fr/CHAP30/ remplacement_fr
 python3 script_xml_normalisation.py ../../../TXM-0.8.0/corpora/EN303136/txm/EN303136/ ../../Travail\ TXM/Corpus/Norm/En/CHAP30/ remplacement_en
